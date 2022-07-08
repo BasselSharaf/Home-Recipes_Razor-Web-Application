a25025f8ebde5650463dd7ec5a55f9ba00477f95
Bassel Amgad <bamgad7@outlook.com>
2022-07-09 01:41:22 +0200

    - Semicolon bug

---
+++ Exercise-3/Pages/Recipes/Edit.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Exercise_3.Pages.Recipes
+{
+    public class EditModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
