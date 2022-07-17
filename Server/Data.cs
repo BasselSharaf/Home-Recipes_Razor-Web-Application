907f80106244c4dc43b3aea4e7a07409f3b89eda
Bassel Amgad <bamgad7@gmail.com>
2022-07-17 02:32:33 +0200

    - Loading and Saving the data is now handled by the Data class not the API

--- Server/Data.cs
+++ Server/Data.cs
@@ -7,7 +7,7 @@ class Data
     private string _recipesFilePath;
     private string _categoriesFilePath;
 
-    public Data ()
+    public Data()
     {
         var systemPath = Environment.GetFolderPath(Environment.SpecialFolder.CommonApplicationData);
         _recipesFilePath = Path.Combine(systemPath, "Recipes.json");
@@ -42,78 +42,98 @@ class Data
         }
     }
 
-    public List<Recipe> GetRecipes()
+    public async Task<List<Recipe>> GetRecipesAsync()
     {
+        await LoadData();
         return _recipes;
     }
 
-    public void AddRecipe(Recipe r)
+    public async Task AddRecipeAsync(Recipe r)
     {
+        await LoadData();
         _recipes.Add(r);
+        await SaveDataAsync();
     }
 
-    public Recipe GetRecipe(Guid id)
+    public async Task<Recipe> GetRecipeAsync(Guid id)
     {
+        await LoadData();
         var recipe = _recipes.Find(r => r.Id == id);
         if (recipe == null)
             recipe = new Recipe();
         return recipe;
     }
 
-    public void RemoveRecipe(Guid id)
+    public async Task RemoveRecipeAsync(Guid id)
     {
-        var recipe = GetRecipe(id); 
+        await LoadData();
+        var recipe = await GetRecipeAsync(id); 
         _recipes.Remove(recipe);
+        await SaveDataAsync();
     }
 
-    public Recipe EditRecipe(Guid id, Recipe newRecipe)
+    public async Task<Recipe> EditRecipeAsync(Guid id, Recipe newRecipe)
     {
-        var recipe = GetRecipe(id);
+        await LoadData();
+        var recipe = await GetRecipeAsync(id);
         recipe.Title = newRecipe.Title;
         recipe.Ingredients = newRecipe.Ingredients;
         recipe.Instructions = newRecipe.Instructions;
         recipe.Categories = newRecipe.Categories;
+        await SaveDataAsync();
         return recipe;
     }
 
-    public void EditTitle(Guid id, string newTitle)
+    public async Task EditTitleAsync(Guid id, string newTitle)
     {
-        var recipe = GetRecipe(id);
+        await LoadData();
+        var recipe = await GetRecipeAsync(id);
         recipe.Title = newTitle;
+        await SaveDataAsync();
     }
 
-    public void EditIngredients(Guid id, string newIngredients)
+    public async Task EditIngredientsAsync(Guid id, string newIngredients)
     {
-        var recipe = GetRecipe(id);
+        await LoadData();
+        var recipe = await GetRecipeAsync(id);
         recipe.Ingredients = newIngredients;
+        await SaveDataAsync();
     }
 
-    public void EditInstructions(Guid id, string newInstructions)
+    public async Task EditInstructionsAsync(Guid id, string newInstructions)
     {
-        var recipe = GetRecipe(id);
+        await LoadData();
+        var recipe = await GetRecipeAsync(id);
         recipe.Instructions = newInstructions;
+        await SaveDataAsync();
     }
     
     //TODO: The only way to edit a category is from the original Method that will edit it inside all recipes
-    public void EditCategory(Guid id, string category, string newCategory)
+    public async Task EditCategoryAsync(Guid id, string category, string newCategory)
     {
-        RemoveCategoryFromRecipe(id, category);
-        AddCategoryToRecipe(id, newCategory);
+        await LoadData();
+        await RemoveCategoryFromRecipeAsync(id, category);
+        await AddCategoryToRecipeAsync(id, newCategory);
+        await SaveDataAsync();
     }
 
-    public List<string> GetAllCategories()
+    public async Task<List<string>> GetAllCategoriesAsync()
     {
+        await LoadData();
         return _categories;
     }
 
-    public void AddCategory(string category)
+    public async Task AddCategoryAsync(string category)
     {
+        await LoadData();
         if(!_categories.Contains(category))
             _categories.Add(category);
+        await SaveDataAsync();
     }
 
-    public void EditCategory(string category, string newCategory)
+    public async Task EditCategoryAsync(string category, string newCategory)
     {
+        await LoadData();
         var index = _categories.IndexOf(category);
         if (index != -1)
         {
@@ -125,10 +145,12 @@ class Data
                         recipe.Categories[recipe.Categories.IndexOf(c)] = newCategory;
                     }
         }
+        await SaveDataAsync();
     }
 
-    public void RemoveCategory(string category)
+    public async Task RemoveCategoryAsync(string category)
     {
+        await LoadData();
         if (_categories.Contains(category))
         {
             _categories.Remove(category);
@@ -136,26 +158,25 @@ class Data
             foreach (var recipe in recipes)
                 recipe.Categories.Remove(category);
         }
+        await SaveDataAsync();
     }
 
-    public void AddCategoryToRecipe(Guid id, string category)
+    public async Task AddCategoryToRecipeAsync(Guid id, string category)
     {
+        await LoadData();
         if (_categories.Contains(category))
         {
-            var recipe = GetRecipe(id);
+            var recipe = await GetRecipeAsync(id);
             recipe.Categories.Add(category);
         }
+        await SaveDataAsync();
     }
 
-    public void RemoveCategoryFromRecipe(Guid id, string category)
+    public async Task RemoveCategoryFromRecipeAsync(Guid id, string category)
     {
-        var recipe = GetRecipe(id);
+        var recipe = await GetRecipeAsync(id);
         recipe.Categories.Remove(category);
-    }
-
-    public async Task WriteToFile(string path, string data)
-    {
-
+        await SaveDataAsync();
     }
 
     public async Task SaveDataAsync()
--- Server/Program.cs
+++ Server/Program.cs
@@ -9,96 +9,79 @@ app.MapGet("", () =>
 app.MapGet("/recipes", async () =>
 {
     Data data = new();
-    await data.LoadData();
-    return Results.Ok(data.GetRecipes());
+    var recipes = await data.GetRecipesAsync();
+    return Results.Ok(recipes);
 });
 
 app.MapGet("/recipes/{id}", async (Guid id) =>
 {
     Data data = new();
-    await data.LoadData();
-    Recipe recipe = data.GetRecipe(id);
+    Recipe recipe = await data.GetRecipeAsync(id);
     return Results.Ok(recipe);
 });
 
 app.MapPost("/recipes", async (Recipe recipe) =>
 {
     Data data = new();
-    await data.LoadData();
     recipe.Id = Guid.NewGuid();
-    data.AddRecipe(recipe);
-    await data.SaveDataAsync();
+    await data.AddRecipeAsync(recipe);
     return Results.Created($"/recipes/{recipe.Id}",recipe);
 });
 
 app.MapPut("/recipes/{id}", async (Guid id, Recipe newRecipe) =>
 {
     Data data = new();
-    await data.LoadData();
-    var updatedRecipe = data.EditRecipe(id, newRecipe);
-    await data.SaveDataAsync();
+    var updatedRecipe =await data.EditRecipeAsync(id, newRecipe);
     return Results.Ok(updatedRecipe);
 });
 
 app.MapDelete("/recipes/{id}", async (Guid id) =>
 {
     Data data = new();
-    await data.LoadData();
-    data.RemoveRecipe(id);
-    await data.SaveDataAsync();
+    await data.RemoveRecipeAsync(id);
     return Results.Ok();
 });
 
 app.MapGet("/categories", async () =>
 {
     Data data = new();
-    await data.LoadData();
-    return Results.Ok(data.GetAllCategories());
+    var categories = await data.GetAllCategoriesAsync();
+    return Results.Ok(categories);
 
 });
 
 app.MapPost("/categories", async (string category) =>
 {
     Data data = new();
-    await data.LoadData();
-    data.AddCategory(category);
-    await data.SaveDataAsync();
+    await data.AddCategoryAsync(category);
     return Results.Created($"/categories/{category}",category);
 });
 
 app.MapPut("/categories", async (string category, string newCategory) =>
 {
     Data data = new();
-    await data.LoadData();
-    data.EditCategory(category, newCategory);
-    await data.SaveDataAsync();
+    await data.EditCategoryAsync(category, newCategory);
     return Results.Ok($"Category ({category}) updated to ({newCategory})");
 });
 
 app.MapDelete("/categories", async (string category) =>
 {
     Data data = new();
-    await data.LoadData();
-    data.RemoveCategory(category);
-    await data.SaveDataAsync();
+    await data.RemoveCategoryAsync(category);
     return Results.Ok();
 });
 
 app.MapPost("recipes/category", async (Guid id ,string category) =>
 {
     Data data = new();
-    await data.LoadData();
-    data.AddCategoryToRecipe(id,category);
-    await data.SaveDataAsync();
+    await data.AddCategoryToRecipeAsync(id,category);
     return Results.Created($"recipes/category/{category}",category);
 });
 
 app.MapDelete("recipes/category", async (Guid id, string category) =>
 {
     Data data = new();
-    await data.LoadData();
-    data.RemoveCategoryFromRecipe(id,category);
-    await data.SaveDataAsync();
+    await data.RemoveCategoryFromRecipeAsync(id,category);
     return Results.Ok();
 });
 
