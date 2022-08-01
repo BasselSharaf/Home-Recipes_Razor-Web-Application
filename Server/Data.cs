cec81d13811c6a2dba34e06df3dbcc0e7ee8effb
Bassel Amgad <bamgad7@gmail.com>
2022-08-01 19:59:58 +0200

    - Added Try&Catch for I/O Operations, and logging

--- Server/Data.cs
+++ Server/Data.cs
@@ -2,43 +2,16 @@
 
 class Data
 {
+    private readonly ILogger _logger;
     private List<Recipe> _recipes { get; set; } = new();
     private List<string> _categories { get; set; } = new();
     private string _recipesFilePath;
     private string _categoriesFilePath;
-
-    public Data()
+    public Data(ILogger logger)
     {
         _recipesFilePath = Path.Combine(Environment.CurrentDirectory, "Data", "Recipes.json");
         _categoriesFilePath = Path.Combine(Environment.CurrentDirectory, "Data", "Categories.json");
-    }
-
-    public async Task LoadData()
-    {
-        if (!File.Exists(_recipesFilePath))
-        {
-            _recipes = new List<Recipe>();
-            await File.WriteAllTextAsync(_recipesFilePath, JsonSerializer.Serialize(_recipes));
-        }
-        if (!File.Exists(this._categoriesFilePath))
-        {
-            _recipes = new List<Recipe>();
-            await File.WriteAllTextAsync(_categoriesFilePath, JsonSerializer.Serialize(_recipes));
-        }
-        using (StreamReader r = new(_recipesFilePath))
-        {
-            var data = await r.ReadToEndAsync();
-            var json = JsonSerializer.Deserialize<List<Recipe>>(data);
-            if (json != null)
-                _recipes = json;
-        }
-        using (StreamReader r = new(_categoriesFilePath))
-        {
-            var data = await r.ReadToEndAsync();
-            var json = JsonSerializer.Deserialize<List<string>>(data);
-            if (json != null)
-                _categories = json;
-        }
+        _logger = logger;
     }
 
     public async Task<List<Recipe>> GetRecipesAsync()
@@ -66,7 +39,7 @@ class Data
     public async Task RemoveRecipeAsync(Guid id)
     {
         await LoadData();
-        var recipe = await GetRecipeAsync(id); 
+        var recipe = await GetRecipeAsync(id);
         _recipes.Remove(recipe);
         await SaveDataAsync();
     }
@@ -106,7 +79,7 @@ class Data
         recipe.Instructions = newInstructions;
         await SaveDataAsync();
     }
-    
+
     public async Task EditCategoryAsync(Guid id, string category, string newCategory)
     {
         await LoadData();
@@ -125,7 +98,7 @@ class Data
     public async Task AddCategoryAsync(string category)
     {
         await LoadData();
-        if(!_categories.Contains(category))
+        if (!_categories.Contains(category))
             _categories.Add(category);
         await SaveDataAsync();
     }
@@ -135,7 +108,7 @@ class Data
         await LoadData();
         if (_categories.Contains(category))
         {
-            foreach(var recipe in _recipes.Where(r => r.Categories.Contains(category)))
+            foreach (var recipe in _recipes.Where(r => r.Categories.Contains(category)))
             {
                 recipe.Categories.Remove(category);
                 recipe.Categories.Add(newCategory);
@@ -152,7 +125,7 @@ class Data
         if (_categories.Contains(category))
         {
             _categories.Remove(category);
-            var recipes =_recipes.Where(r => r.Categories.Contains(category)).ToList();
+            var recipes = _recipes.Where(r => r.Categories.Contains(category)).ToList();
             foreach (var recipe in recipes)
                 recipe.Categories.Remove(category);
         }
@@ -177,9 +150,60 @@ class Data
         await SaveDataAsync();
     }
 
+    public async Task LoadData()
+    {
+        // Creating the files if they do not exist
+        try
+        {
+            if (!File.Exists(_recipesFilePath))
+            {
+                _recipes = new List<Recipe>();
+                await File.WriteAllTextAsync(_recipesFilePath, JsonSerializer.Serialize(_recipes));
+            }
+            if (!File.Exists(this._categoriesFilePath))
+            {
+                _recipes = new List<Recipe>();
+                await File.WriteAllTextAsync(_categoriesFilePath, JsonSerializer.Serialize(_recipes));
+            }
+        }
+        catch (Exception ex)
+        {
+            _logger.LogError(DateTime.Now.ToString(), "- Error while attempting to create the files: ", ex.ToString());
+        }
+        try
+        {
+            using (StreamReader r = new(_recipesFilePath))
+            {
+                var data = await r.ReadToEndAsync();
+                var json = JsonSerializer.Deserialize<List<Recipe>>(data);
+                if (json != null)
+                    _recipes = json;
+            }
+            using (StreamReader r = new(_categoriesFilePath))
+            {
+                var data = await r.ReadToEndAsync();
+                var json = JsonSerializer.Deserialize<List<string>>(data);
+                if (json != null)
+                    _categories = json;
+            }
+        }
+        catch (Exception ex)
+        {
+            _logger.LogError(DateTime.Now.ToString(), "- Error while attempting to save the data to the files: ", ex.ToString());
+        }
+
+    }
+
     public async Task SaveDataAsync()
     {
-        await File.WriteAllTextAsync(_recipesFilePath, JsonSerializer.Serialize(_recipes));
-        await File.WriteAllTextAsync(_categoriesFilePath, JsonSerializer.Serialize(_categories));
+        try
+        {
+            await File.WriteAllTextAsync(_recipesFilePath, JsonSerializer.Serialize(_recipes));
+            await File.WriteAllTextAsync(_categoriesFilePath, JsonSerializer.Serialize(_categories));
+        }
+        catch (Exception ex)
+        {
+            _logger.LogError(DateTime.Now.ToString(), "- Error while attempting to save files: ", ex.ToString());
+        }
     }
 }
--- Server/Program.cs
+++ Server/Program.cs
@@ -1,4 +1,3 @@
-using Microsoft.OpenApi.Models;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -11,7 +10,7 @@ builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
-
+app.Logger.LogInformation("The app started");
 
 app.UseSwagger();
 app.UseSwaggerUI();
@@ -27,23 +26,23 @@ app.UseSwaggerUI(options =>
     options.RoutePrefix = string.Empty;
 });
 
-app.MapGet("/recipes", async () =>
+app.MapGet("recipes", async () =>
 {
-    Data data = new();
+    Data data = new(app.Logger);
     var recipes = await data.GetRecipesAsync();
     return Results.Ok(recipes);
 });
 
 app.MapGet("/recipes/{id}", async (Guid id) =>
 {
-    Data data = new();
+    Data data = new(app.Logger);
     Recipe recipe = await data.GetRecipeAsync(id);
     return Results.Ok(recipe);
 });
 
 app.MapPost("/recipes", async (Recipe recipe) =>
 {
-    Data data = new();
+    Data data = new(app.Logger);
     recipe.Id = Guid.NewGuid();
     await data.AddRecipeAsync(recipe);
     return Results.Created($"/recipes/{recipe.Id}",recipe);
@@ -51,21 +50,21 @@ app.MapPost("/recipes", async (Recipe recipe) =>
     
 app.MapPut("/recipes/{id}", async (Guid id, Recipe newRecipe) =>
 {
-    Data data = new();
+    Data data = new(app.Logger);
     var updatedRecipe =await data.EditRecipeAsync(id, newRecipe);
     return Results.Ok(updatedRecipe);
 });
 
 app.MapDelete("/recipes/{id}", async (Guid id) =>
 {
-    Data data = new();
+    Data data = new(app.Logger);
     await data.RemoveRecipeAsync(id);
     return Results.Ok();
 });
 
 app.MapGet("/categories", async () =>
 {
-    Data data = new();
+    Data data = new(app.Logger);
     var categories = await data.GetAllCategoriesAsync();
     return Results.Ok(categories);
 
@@ -73,35 +72,35 @@ app.MapGet("/categories", async () =>
 
 app.MapPost("/categories", async (string category) =>
 {
-    Data data = new();
+    Data data = new(app.Logger);
     await data.AddCategoryAsync(category);
     return Results.Created($"/categories/{category}",category);
 });
 
 app.MapPut("/categories", async (string category, string newCategory) =>
 {
-    Data data = new();
+    Data data = new(app.Logger);
     await data.EditCategoryAsync(category, newCategory);
     return Results.Ok($"Category ({category}) updated to ({newCategory})");
 });
 
 app.MapDelete("/categories", async (string category) =>
 {
-    Data data = new();
+    Data data = new(app.Logger);
     await data.RemoveCategoryAsync(category);
     return Results.Ok();
 });
 
 app.MapPost("recipes/category", async (Guid id ,string category) =>
 {
-    Data data = new();
+    Data data = new(app.Logger);
     await data.AddCategoryToRecipeAsync(id,category);
     return Results.Created($"recipes/category/{category}",category);
 });
 
 app.MapDelete("recipes/category", async (Guid id, string category) =>
 {
-    Data data = new();
+    Data data = new(app.Logger);
     await data.RemoveCategoryFromRecipeAsync(id,category);
     return Results.Ok();
 });
