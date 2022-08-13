74572edca1ba5108252f16de9528f8d465a9af0a
Bassel Amgad <bamgad7@gmail.com>
2022-08-13 20:46:17 +0200

    - Filter according to selected Category.

--- Client/Pages/Recipes/Index.cshtml.cs
+++ Client/Pages/Recipes/Index.cshtml.cs
@@ -7,14 +7,23 @@ namespace Exercise3.Pages.Recipes
     {
         private readonly IHttpClientFactory _httpClientFactory;
         public List<Recipe> Recipes { get; set; } = new();
+        public string? ChoosenCategory { get; set; }
         public IndexModel(IHttpClientFactory httpClientFactory) => _httpClientFactory = httpClientFactory;
 
-        public async Task OnGetAsync()
+        public async Task OnGetAsync(string? category)
         {
             var client = _httpClientFactory.CreateClient("Recipes");
             var fetchRecipes = await client.GetFromJsonAsync<List<Recipe>>("recipes");
             if (fetchRecipes is not null)
+            {
+                if (category is not null)
+                {
+                    fetchRecipes = fetchRecipes.Where(r => r.Categories.Contains(category)).ToList();
+                    ChoosenCategory = category;
+                }
                 Recipes = fetchRecipes;
+            }
+                
         }
     }
 }
